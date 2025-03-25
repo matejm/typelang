c002dce00752af68c84da8a93df7612ac78106d5
Matej Marinko <matej@zerodays.dev>
2025-03-25 21:15:43 +0100

    feat: initial version

---
+++ examples.ts
@@ -0,0 +1,35 @@
+import { Execute, Instruction } from "./typelang";
+
+type addStrings = Execute<
+  [
+    Instruction.Extend<"a">,
+    Instruction.Extend<"b">,
+    Instruction.Extend<"c">,
+    Instruction.Extend<"d">
+  ]
+>;
+// type addStrings = "abcd"
+
+type getThirdCharUppercase = Execute<
+  [
+    Instruction.Extend<"abcd">,
+    Instruction.RemoveFirstChar,
+    Instruction.RemoveFirstChar,
+    Instruction.Uppercase,
+    Instruction.FirstChar
+  ]
+>;
+// type getThirdCharUppercase = "C"
+
+type exception = Execute<
+  [
+    Instruction.Extend<"ab">,
+    Instruction.RemoveFirstChar,
+    Instruction.RemoveFirstChar,
+    Instruction.RemoveFirstChar
+  ]
+>;
+// type exception = {
+//     message: "Empty string, cannot remove first character";
+//     instruction: Instruction.RemoveFirstChar;
+// }
---
+++ typelang.ts
@@ -0,0 +1,70 @@
+export namespace Instruction {
+  // String concatenation
+  export type Extend<Value extends string> = { type: "extend"; value: Value };
+
+  // Convert string to uppercase / lowercase
+  export type Uppercase = { type: "uppercase" };
+  export type Lowercase = { type: "lowercase" };
+
+  // Get first character / remove first character
+  export type FirstChar = { type: "car" };
+  export type RemoveFirstChar = {
+    type: "cdr";
+  };
+
+  export type AvailableInstructions =
+    | Extend<string>
+    | Uppercase
+    | Lowercase
+    | FirstChar
+    | RemoveFirstChar;
+}
+
+export type Exception<
+  Message extends string,
+  Instruction extends Instruction.AvailableInstructions
+> = {
+  // Error message
+  message: Message;
+
+  // Where the exception occurred.
+  instruction: Instruction;
+};
+
+// Evaluate single instruction
+export type Evaluate<
+  Instruction extends Instruction.AvailableInstructions,
+  Value extends string | Exception<string, Instruction.AvailableInstructions>
+> = Value extends string
+  ? // Instructions
+    Instruction extends Instruction.Extend<infer ToExtend>
+    ? `${Value}${ToExtend}`
+    : Instruction extends Instruction.Uppercase
+    ? Uppercase<Value>
+    : Instruction extends Instruction.Lowercase
+    ? Lowercase<Value>
+    : Instruction extends Instruction.FirstChar
+    ? Value extends `${infer First}${infer Rest}`
+      ? First
+      : Exception<"Empty string, cannot get first character", Instruction>
+    : Instruction extends Instruction.RemoveFirstChar
+    ? Value extends `${infer First}${infer Rest}`
+      ? Rest
+      : Exception<"Empty string, cannot remove first character", Instruction>
+    : Exception<"Unknown instruction", Instruction>
+  : // Value is exception, pass it through
+    Value;
+
+// Execute program
+// (a list of instructions)
+export type Execute<
+  Instructions extends Instruction.AvailableInstructions[],
+  Value extends
+    | string
+    | Exception<string, Instruction.AvailableInstructions> = ""
+> = Instructions extends [
+  infer First extends Instruction.AvailableInstructions,
+  ...infer Rest extends Instruction.AvailableInstructions[]
+]
+  ? Execute<Rest, Evaluate<First, Value>>
+  : Value;
